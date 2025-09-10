0699ff37ddc97601e03c4695a17382d222a880f1
kdongsu5509 <kod66170@gmail.com>
2025-09-10 21:33:12 +0900

    hotfix/ add allowed origin url to avoid cor

--- Backend/src/main/java/com/halfmoon/halfmoon/security/global/config/SecurityConfig.java
+++ Backend/src/main/java/com/halfmoon/halfmoon/security/global/config/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000", "http://localhost:5500", "http://localhost:8080", "http://127.0.0.1:5500"
+                "http://localhost:3011", "http://localhost:3000", "http://localhost:5500", "http://localhost:8080",
+                "http://127.0.0.1:5500"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Collections.singletonList("*"));
