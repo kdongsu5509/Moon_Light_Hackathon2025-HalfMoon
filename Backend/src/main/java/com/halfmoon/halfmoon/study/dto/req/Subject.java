afaf45033df4258b1c9592990a667fc6bea2dcfb
kdongsu5509 <kod66170@gmail.com>
2025-09-13 21:33:55 +0900

    hotfix/ update introduction to SELFINTRODUCTION

--- Backend/src/main/java/com/halfmoon/halfmoon/study/dto/req/Subject.java
+++ Backend/src/main/java/com/halfmoon/halfmoon/study/dto/req/Subject.java
@@ -1,7 +1,7 @@
 package com.halfmoon.halfmoon.study.dto.req;
 
 public enum Subject {
-    INTRODUCTION, // 자기소개
+    SELFINTRODUCTION, // 자기소개
     FAMILY, // 가족
     SCHOOL, // 학교
     FOOD, // 음식
