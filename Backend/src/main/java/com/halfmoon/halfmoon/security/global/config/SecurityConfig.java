8e617eb1157401a769ab2a6ce545e05ada137cc1
kdongsu5509 <kod66170@gmail.com>
2025-09-13 23:56:31 +0900

    hotfix

--- Backend/src/main/java/com/halfmoon/halfmoon/security/global/config/SecurityConfig.java
+++ Backend/src/main/java/com/halfmoon/halfmoon/security/global/config/SecurityConfig.java
@@ -76,7 +76,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3001", "http://localhost:3011", "http://localhost:3000", "http://localhost:5500",
                 "http://localhost:8080",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "http://43.201.95.54"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Collections.singletonList("*"));
