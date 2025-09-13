78448cfe2ab1ea93c69a18a3d65e7c144520b2b7
kdongsu5509 <kod66170@gmail.com>
2025-09-13 21:16:30 +0900

    hotfix : update api

--- Backend/src/main/java/com/halfmoon/halfmoon/study/presentation/ChatController.java
+++ Backend/src/main/java/com/halfmoon/halfmoon/study/presentation/ChatController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,7 +44,7 @@ public class ChatController {
                     )
             }
     )
-    @GetMapping("/start")
+    @PostMapping("/start")
     public APIResponse<ConversationStartResponse> startConversation(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody ConversationStartRequest request) {
